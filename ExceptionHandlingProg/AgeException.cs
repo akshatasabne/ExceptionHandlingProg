c59112731bd2a399e5273267850a8d532783bf36
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-12 14:04:59 +0530

    Add project files.

---
+++ ExceptionHandlingProg/AgeException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingProg
+{
+    public class AgeException:Exception
+    {
+        
+            public AgeException(string message) : base(message)
+            {
+
+            }
+   
+
+        public class Student
+        {
+            private string name;
+            private int age;
+
+            public void AcceptDetails(string name, int age)
+            {
+               
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new AgeException("Name is required");
+                }
+                else
+                {
+                    this.name = name;
+                    this.age = age;
+                }
+
+            }
+        }
+    }
+}
---
+++ ExceptionHandlingProg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ExceptionHandlingProg.AgeException;
+
+namespace ExceptionHandlingProg
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+            try
+            {
+                stud.AcceptDetails("",19);
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+
+        }
+    }
+}
